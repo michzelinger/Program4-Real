b38de22c8c98f1f57214b4cb3431bb432356bb12
Michael Zelinger <mzelin@csslab8.uwb.edu>
2021-03-01 13:53:30 -0800

    main template

---
+++ Program4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(/*int argc, char * const argv[]*/)
+{
+   /*
+   if(argc != 3)
+   {
+      cerr << "Not enough arguments passed in. It is expected that 3
+      arguments (including execution file) will be passed in." << endl;
+      return 0;
+   }
+   int population = 0;
+   int generations = 0;
+   popultaion = atoi(argv[1]);
+   generations = atoi(argv[2]);*/
+   
+   string initialSudokuPuzzle;
+   cout << "Please Insert 81 characters for the initial puzzle. " << endl;
+   cout << "Insert Here: ";
+   cin >> initialSudokuPuzzle;
+   cout << endl;
+   return 0;
+}
