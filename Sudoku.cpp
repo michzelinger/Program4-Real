3b04dca7def080f6516dfae0e56786283a76d4b9
michzelinger <mzelin@uw.edu>
2021-03-10 18:30:01 -0800

    Update Sudoku.cpp

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -1,21 +1,21 @@
 #include "Sudoku.h"
 #include "Element.h"
-  Sudoku::Sudoku() {
-    sudokuGrid_.resize(81);
-  }
-  Sudoku::~Sudoku(){
+Sudoku::Sudoku() {
+   sudokuGrid_.resize(81);
+}
+Sudoku::~Sudoku() {
 
-  }
+}
 
 void Sudoku::setValue(int r, int c, int val, bool availability) {
-    
-    if(availability == true){
-      sudokuGrid_[(r-1)*9+c-1] = Element(val,availability);
-    } 
-    else {
-      cerr << "Value cant be changed" << endl;
-    }
-     
+
+   
+   sudokuGrid_[(r - 1) * 9 + c - 1] = Element(val, availability);
+   
+   // else {
+   //   cerr << "Value cant be changed" << endl;
+   // }
+
 }
 
 //  int Sudoku::value(int r, int c) {
@@ -29,60 +29,60 @@ void Sudoku::setValue(int r, int c, int val, bool availability) {
 // void Sudoku::setValue(int r, int c, int val, bool deter) {
 //     grid[r*9+c] = {val, deter};
 // }
-istream &Sudoku::readin(istream &in) {
-    // first clear grid
-    bool availability;
-    sudokuGrid_.clear();
-    char c;
-    while (in >> c) {
-        // read finished, break
-   
-                // only use in digit
-        if (isdigit(c)) {
-          if(c-'0' > 0){
-            cout << "here" <<endl;
+istream& Sudoku::readin(istream& in) {
+   // first clear grid
+   bool availability;
+   sudokuGrid_.clear();
+   char c;
+   while (in >> c) {
+      // read finished, break
+
+              // only use in digit
+      if (isdigit(c)) {
+         if (c - '0' > 0) {
+            cout << "here" << endl;
             availability = false;
-          }
-          else{
+         }
+         else {
             cout << "here 2" << endl;
             availability = true;
-          }
-          Element e(c-'0',availability);
-          sudokuGrid_.push_back(e);
+         }
+         Element e(c - '0', availability);
+         sudokuGrid_.push_back(e);
 
          // cout <<sudokuGrid_.size() <<endl;
-          cout <<sudokuGrid_.size() <<endl;
-        }
-        if (sudokuGrid_.size() == 81) {
-           break;
-        }
-    }
-    return in;
+         cout << sudokuGrid_.size() << endl;
+      }
+      if (sudokuGrid_.size() == 81) {
+         break;
+      }
+   }
+   return in;
 }
 
-ostream &Sudoku::printout(ostream &out) const {
+ostream& Sudoku::printout(ostream& out) const {
 
-    // header and footer string    
-    const string hdr_ftr = "+-------+-------+-------+\n";
-    out << hdr_ftr;
-    for (int i=0; i<9; ++i) {
-        // border
-        out << "|";
-        for (int j=0; j<9; ++j) {
-            out << " " << sudokuGrid_[i*9+j].getValue();
-            // inner border
-            if (j % 3 == 2) out << " |";
-        }
-        out << endl;
-        if (i % 3 == 2) out << hdr_ftr;
-    }
-    return out;
+   // header and footer string    
+   const string hdr_ftr = "+-------+-------+-------+\n";
+   out << hdr_ftr;
+   for (int i = 0; i < 9; ++i) {
+      // border
+      out << "|";
+      for (int j = 0; j < 9; ++j) {
+         out << " " << sudokuGrid_[i * 9 + j].getValue();
+         // inner border
+         if (j % 3 == 2) out << " |";
+      }
+      out << endl;
+      if (i % 3 == 2) out << hdr_ftr;
+   }
+   return out;
 }
 
-bool Sudoku::getChangeAvailability(int row, int column){
-  return sudokuGrid_[(row-1)*9+column-1].getAvalability();
+bool Sudoku::getChangeAvailability(int row, int column) {
+   return sudokuGrid_[(row - 1) * 9 + column - 1].getAvalability();
 }
 
-int Sudoku::getValue(int row, int column){
-  return sudokuGrid_[(row-1)*9+column-1].getValue();
+int Sudoku::getValue(int row, int column) {
+   return sudokuGrid_[(row - 1) * 9 + column - 1].getValue();
 }
